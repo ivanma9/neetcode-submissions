2909ed91313925482961023d9685c1218ee7227d
Ivan Ma <43893062+ivanma9@users.noreply.github.com>
2026-09-21 12:55:21 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_parsed = s.upper()
+        l,r = 0, len(s_parsed) - 1
+        alphanum = set()
+        for i in range(0,10):
+            alphanum.add(str(i))
+        for i in range(0,26):
+            alphanum.add(chr(i+ 65))
+        print(s_parsed)
+
+        while(l<r):
+
+            while l < r and s_parsed[l] not in alphanum:
+
+                print("m from left")
+                l+=1
+            while l < r and s_parsed[r] not in alphanum:
+                print("m from right")
+                r-=1
+            if s_parsed[l] == s_parsed[r]:
+                l+=1
+                r-=1
+            else:
+                return False
+            
+
+
+        return True
