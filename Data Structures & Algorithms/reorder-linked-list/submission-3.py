3c844a798b3b285df6ad6cbfab319e21ac7a20d7
Ivan Ma <43893062+ivanma9@users.noreply.github.com>
2026-09-23 23:35:13 -0700

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.py
@@ -0,0 +1,58 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # head
+        # [0 1 2 3 4 5 6]
+
+        def merge_two_lists(l1,l2):
+            l1Turn = True
+            # dummy = ListNode()
+            # dummy.next = l1
+            while(l1 and l2):
+                if l1Turn:
+                    
+                    nxtNode = l1.next
+                    l1.next = l2
+                    l1 = nxtNode
+                        
+                else:
+                    
+                    nxtNode = l2.next
+                    l2.next = l1
+                    l2 = nxtNode
+                l1Turn = not l1Turn
+            return l1
+            
+
+
+
+        # find half way
+        slow = head
+        fast = head.next
+        while(fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+
+        cur = slow.next
+
+        slow.next = None
+    
+        # reverse second half
+        prev = None
+        while(cur):
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        
+
+        # merge 2 lists
+        head = merge_two_lists(head, prev)
+        
+        
+        
